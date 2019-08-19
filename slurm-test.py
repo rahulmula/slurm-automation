175e980f7eea6a3ecb1dce03ecadb3248f5722f0
rahulmula <rahul.mulag@gmail.com>
2019-08-19 11:29:02 +0530

    automation commit

--- slurm-test.py
+++ slurm-test.py
@@ -1,7 +1,13 @@
-from prettytable import PrettyTable
-import os, sys, re, itertools
+#from prettytable import PrettyTable
+import os, sys, re, itertools, subprocess, json
+from subprocess import PIPE, run
 
-gpu_logpath = "/home/taccuser/test-automation/out_gpu.log"
+
+gpu_logpath = "/home/taccuser/slurm-automation/slurm-automation/out_gpu.log"
+srun_command = "sudo srun -N 1 --gres=gpu:2 hostname"
+autodetect_command = "salloc -N 1 --gres=gpu:2 --begin=now --time=10"
+regex=r'^node-name:.*$'
+
 
 num_gpus = 0
 with open(gpu_logpath, "r") as file:
@@ -9,3 +15,55 @@ with open(gpu_logpath, "r") as file:
         num_gpus += 1
 print("Number of GPUs:")
 print(num_gpus)
+
+def cmdline(command):
+    process = Popen(args=command, stdout=PIPE, shell=True)
+    return process.communicate()[0]
+
+
+def slurm_gpudetect():
+    res = os.popen("%s" %srun_command).read()
+    res_out = res.strip() #strip: removes spaces from output
+    file1 = open("test-server.log","r")
+    var = re.findall(r'node-name:(.*?)(?=\s*,\s*node-name:|$)', file1.read())    
+    str1 = ''.join(var)
+    if res_out == str1:
+        print("Pass")
+    else:               
+        print("Fail")
+
+def slurm_gpu_autodetect():
+    res = os.popen("%s >/dev/tty" %autodetect_command).read()
+    #res = run(autodetect_command, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
+    #res = subprocess.check_output(srun_command, shell=True).read()
+    #print(res)
+
+def slurm_load_conf(var):
+    config = json.loads(open('/home/taccuser/slurm-automation/slurm-automation/conf.json').read())
+    return config["%s" %var]
+    #print(config["%s" %var])
+
+def slurm_automatic_allocation(n1hostname,n1autoallocation_command):
+    n1host = slurm_load_conf(n1hostname)
+    n1_autoallocation = slurm_load_conf(n1autoallocation_command)
+    allocate  =  os.popen("%s" %n1_autoallocation).read()
+    res = allocate.strip()
+    #print(res)
+    if n1host == res:
+        print("Pass")
+    else:
+        print("Fail")
+
+def slurm_autonode_allocation():
+    slurm_automatic_allocation("N1hostname","N1_autoallocation")
+    nodeexist = slurm_load_conf("N2hostname")
+    if nodeexist != "None":
+        print("Multi-Node exist!")
+        slurm_automatic_allocation("N2hostname","N2_autoallocation")        
+    else:
+        print("Multi-Node not exist!")
+
+#slurm_gpudetect()
+#slurm_gpu_autodetect() 
+#slurm_load_conf()
+slurm_autonode_allocation()
