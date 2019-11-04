13044ef9b291a20cb1db5aeaf41877d7ce576784
rahulmula <rahul.mulag@gmail.com>
2019-11-04 16:25:47 +0530

    latest changes added

--- slurm-test.py
+++ slurm-test.py
@@ -1,4 +1,4 @@
-import os, sys, re, itertools, subprocess, json
+import os, sys, re, itertools, subprocess, json, logging
 from subprocess import PIPE, run
 from prettytable import PrettyTable
 
@@ -8,6 +8,10 @@ autodetect_singlenode_command="salloc -N 1 --gres=gpu:1 --begin=now --time=10"
 node_cancel_regex=r'.*?Granted.*$'
 x = PrettyTable()
 x.field_names = ["Slurm Test Scenarious","Result"]
+logging.basicConfig(filename="/home/taccuser/slurm.log", format='%(asctime)s %(message)s', filemode='w')
+logger=logging.getLogger()
+logger.setLevel(logging.DEBUG)
+logfile = open("/home/taccuser/slurm.log",'w')
 
 
 
@@ -29,34 +33,47 @@ def slurm_group_gpudetection():
                 print("pass")
         
 
+
 def slurm_master_setup():
-    res = os.popen("sinfo").read()    
+    res = os.popen("sinfo").read()
     if "NODELIST" in res:        
         x.add_row(["Slurm Master Setup", "Pass"])
+        result = 'Successful !!'
     else:
         x.add_row(["Slurm Master Setup", "Fail"])
+        result = 'Failed !'
+    logfile.write('TestCase 1) Slurm master setup: \n\n' + res + '\n')  
+    logfile.write('Slurm Master Setup:' + result + ' \n')
+
 
 
 def slurm_node_setup():
     res = os.popen("sinfo").read()
     nodename = slurm_load_conf("N1hostname")
     if nodename in res:        
-        x.add_row(["Slurm Node Setup", "Pass"])
+        x.add_row(["Slurm Node Setup", "Pass"])        
+        result = 'Successful !!'
     else:    
         x.add_row(["Slurm Node Setup", "Fail"])
-  
+        result = 'Failed !'        
+    logfile.write('\nTestCase 2) Slurm Node Setup: \n\n' + res + '\n' )
+    logfile.write('Slurm Node Setup:' + result + ' \n')
+
 
 
 def slurm_gpu_detect():
-    nodename = slurm_load_conf("N1hostname")
-    res=os.popen("srun -w %s /opt/rocm/bin/rocm_agent_enumerator" %nodename).read()
+    nodename = slurm_load_conf("N1hostname")    
+    res=os.popen("srun -w %s /opt/rocm/bin/rocm_agent_enumerator" %nodename).read()    
     slurm_gpu_detected =  int(res.count("gfx") -1)
-    node_gpu_devices = int(slurm_load_conf("N1cards"))
-    
+    node_gpu_devices = int(slurm_load_conf("N1cards"))    
     if (node_gpu_devices == slurm_gpu_detected):
         x.add_row(["Total No.of Gpu vs Slurm No.of Gpu detecting", "Pass"])
+        result = 'Successful !!'        
     else:
         x.add_row(["Total No.of Gpu vs Slurm No.of Gpu detecting", "Fail"])
+        result = 'Failed !'
+    logfile.write('\nTestCase 3) Slurm Gpu Detect: \n\n Output\n' + res + '\nTotal No.Of Gpu detected using Slurm:' + str(slurm_gpu_detected) + '\nTotal Gpu in the Node :' + str(node_gpu_devices) )
+    logfile.write('\nTotal No.of Gpu vs Slurm No.of Gpu detecting is :' + result +' \n')
 
 
 
@@ -72,28 +89,39 @@ def cancel_allocation():
                 os.popen("scancel %s" %str[0])
 
 
-def validate_output():
-    print("you have not entered")
+
+def validate_output():    
     try:        
         with open("/home/taccuser/slurm-automation/allocation.log", "r") as file:
         #with open(slurm_load_conf("node_cancel_regex"), "r") as file:        
             if "error" in file.read():           
-                x.add_row(["GPU autodetect single-node allocation", "Fail"])            
+                x.add_row(["Slurm node allocation with GPU auto detect", "Fail"])
+                result='Failed !'
+                #f = open("/home/taccuser/slurm.log","r")
+                #if f.mode == 'r':                                     
+                f = open('/home/taccuser/slurm-automation/allocation.log', 'r')
+                contents = f.read()
+                print(contents)
+                logfile.write('\n\n Encountered Error:\n' + contents )
+                #f.close()
             else:
-                x.add_row(["GPU autodetect single-node allocation", "Pass"])
-                str = os.popen("sinfo").read()
-            
+                x.add_row(["Slurm node allocation with GPU auto detect", "Pass"])
+                result='Successful !!'
+                str1 = os.popen("sinfo").read()
+            logfile.write( str1 + ' \n\nSlurm node allocation with GPU auto detect:' + result +' \n')
     except:
         pass
                 
+
 def slurm_node_allocation(autodetect_command):
     try:        
         res = subprocess.run("%s 2>&1 | tee allocation.log >/dev/tty" %autodetect_command, timeout=2, shell=True)        
-        validate_output()
+        logfile.write('\nTestCase 4) Slurm Node Allocation: \n\nOutput:' + res)
+        validate_output()        
     except:
-        pass       
-        
+        pass      
         validate_output()
+
 
 
 def slurm_auto_allocation(n1hostname,n1autoallocation_command):
@@ -103,14 +131,20 @@ def slurm_auto_allocation(n1hostname,n1autoallocation_command):
     allocate = subprocess.run("%s" %n1_autoallocation, stdout=subprocess.PIPE, timeout=5, shell=True)    
     var = allocate.stdout.decode()
     res = var.strip()
+    logfile.write('output:' + var)
     if n1host == res:    
         x.add_row(["slurm job  with --gpu flag", "Pass"])       
+        result='Successful !!'
     else:        
-        x.add_row(["slurm job with --gpu flag", "Fail"])        
+        x.add_row(["slurm job with --gpu flag", "Fail"])
+        result='Failed !'
+    logfile.write('\nSlurm job with --gpu flag:' + result +' \n')
+
 
 
 def slurm_autonode_allocation():
     try:
+        logfile.write('\nTestCase 4) Slurm autonode allocation: \n')
         slurm_auto_allocation("N1hostname","N1_autoallocation")
         nodeexist = slurm_load_conf("N2hostname")        
         print("N2 has error")
@@ -122,21 +156,28 @@ def slurm_autonode_allocation():
     except:
         pass
 
+
+
 def slurm_gpu_separation():
     try:
         nodename = slurm_load_conf("N1hostname")
         res=os.popen("srun -w %s --gres=gpu:1 /opt/rocm/bin/rocm_agent_enumerator" %nodename).read()
         slurm_gpu_detected =  int(res.count("gfx") -1)
+
         if slurm_gpu_detected == 1:            
             x.add_row(["Gpu segregation", "Pass"])
+            result='Successful !!'
         else:            
             x.add_row(["Gpu segregation", "Fail"])
+            result='Failed !'
+        logfile.write('\nTestCase 3) Slurm GPU segregation: \n\n Command:srun -w %s --gres=gpu:1 /opt/rocm/bin/rocm_agent_enumerator  \nOutput\n' + res + '\nTotal No.Of Gpu matched \nGpu segregation' + result  )
 
     except:
         pass
 
 
 #slurm_group_gpudetection()
+
 
 
 slurm_master_setup()
@@ -148,6 +189,7 @@ cancel_allocation()
 slurm_autonode_allocation()
 slurm_gpu_separation()
 print(x)
+print("For reference pls find the log file at ~/slurm.log") 
 
 
 
